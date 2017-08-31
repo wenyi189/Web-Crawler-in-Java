45a16672b66750a86b361a36ec639ea8471e6b21
Yi Wen <yi@amberengine.com>
2017-08-31 17:51:09 -0400

    spider

--- src/Crawler/Spider.java
+++ src/Crawler/Spider.java
@@ -1,5 +1,62 @@
 package Crawler;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Spider {
+
+    private static final int MAX_PAGES_TO_SEARCH = 100;
+    private Set<String> pageVisited = new HashSet<>();
+    private List<String> pageToVisit = new LinkedList<>();
+
+    /**
+     * To determine the next URL to visit
+     * if the URL is already visited, skip it
+     * and not, get the URL and save it to pageVisited
+     *
+     * @return nextUrl
+     */
+    private String nextUrl() {
+        String nextUrl;
+
+        do {
+            nextUrl = this.pageToVisit.remove(0);
+        } while(this.pageVisited.contains(nextUrl));
+
+        this.pageVisited.add(nextUrl);
+        return nextUrl;
+    }
+
+    /**
+     *
+     * @param url
+     * @param searchWord
+     */
+    public void search(String url, String searchWord) {
+        while(this.pageVisited.size() < MAX_PAGES_TO_SEARCH) {
+            String currentUrl;
+            SpiderLeg leg = new SpiderLeg();
+
+            if (this.pageVisited.isEmpty()) {
+                currentUrl = url;
+                this.pageVisited.add(currentUrl);
+            } else {
+                currentUrl = nextUrl();
+            }
+
+            leg.crawl(currentUrl);
+            boolean success = leg.searchForWord(searchWord);
+
+            if(success) {
+                System.out.println("**Success** Word " + searchWord + "found at " + currentUrl);
+                break;
+            } else {
+                this.pageToVisit.addAll(leg.getLinks());
+            }
+        }
+        System.out.println("**Done** Visited " + this.pageVisited.size() + "web page(s)");
+    }
 
 }
---
+++ src/Crawler/SpiderLeg.java
@@ -0,0 +1,31 @@
+package Crawler;
+
+import java.util.List;
+
+public class SpiderLeg {
+
+    /**
+     *
+     * @param nextUrl
+     */
+    public void crawl(String nextUrl) {
+
+    }
+
+    /**
+     *
+     * @param searchWord
+     * @return
+     */
+    public boolean searchForWord(String searchWord) {
+
+    }
+
+    /**
+     *
+     * @return List of url on the page
+     */
+    public List<String> getLinks() {
+
+    }
+}
